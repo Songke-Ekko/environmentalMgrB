70f80ebb102016c9fc77f8c38b06886b51d70fa2
rurujiadedashuaiguo <13938338016@163.com>
2020-11-01 16:13:05 +0800

    轮播图提交

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,49 @@
 import React, { Fragment } from 'react';
+import tree from '@/assets/tree.svg';
+import styles from './index.less'
 
-const Header = (props) => {
+const Header = () => {
   return (
     <Fragment>
-      123
+      <div className={styles.mainBox}>
+        <div className={styles.title}>
+          <img src={tree} />
+          <span>环境管理系统B期</span>
+        </div>
+        <div className={styles.slogan}>
+          <span>请您爱护绿色，绿是生命之源</span>
+        </div>
+        <ul className={styles.headerUl}>
+          <li className="login">
+            <span className="member-name">您好，请登录</span>
+          </li>
+          {/* <li
+            className="login login-username"
+          >
+            <span className="member-name login-username-title"></span>
+          </li> */}
+          {/* <li className="sign-out">
+            <span className="sign-out-leave">退出</span>
+            <span className="sign-out-editPsw">修改密码</span>
+          </li> */}
+          <li>
+              <span className="register">免费注册</span>
+          </li>
+          <li>
+              <span className="register">服务中心</span>
+          </li>
+          <li className="header-phone">
+            <a href="#">
+              {/* <img src="../../static/images/手机.png" className="phone-icon" /> */}
+            </a>
+          </li>
+          <li className="header-weixin">
+            <a href="#">
+              {/* <img src="../assets/微信.png" className="weixin-icon" /> */}
+            </a>
+          </li>
+        </ul>
+      </div>
     </Fragment>
   );
 };
--- src/pages/WorkSpace/index.js
+++ src/pages/WorkSpace/index.js
@@ -1,10 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from '@/components/Header';
+import { Carousel } from 'antd';
+import heart from '@/assets/xinOnce.gif';
+import styles from './index.less';
+import green from '@/assets/green.jpg';
+import green1 from '@/assets/green1.jpg';
+import green2 from '@/assets/green2.jpg';
+import green3 from '@/assets/green3.jpg';
+import green4 from '@/assets/green4.jpg';
+
+const imgList = [green, green1, green2, green3, green4];
 
 const WorkSpace = (props) => {
+  const [vislble1, setVislble1] = useState(false);
+  const [vislble, setVislble] = useState(false);
+  const [array, setArray] = useState([]);
+
+  const parents = () => {
+    setVislble1(!vislble1);
+    setVislble(true);
+    // setTimeout(() => {
+    //   setVislble(false);
+    // }, 2000);
+  }
+
+  const arrayAdd = () => {
+    array.push(1)
+    setArray([...array]);
+  }
+
+  useEffect(() => {
+    console.log(imgList);
+  }, [])
+  
   return (
     <Fragment>
-      <div>
-        123456
+      <Header />
+      {/* <div onClick={parents} style={{ border: '1px solid', height: 100, width: 100 }}>
+        {
+          !vislble1 ? <div className={styles.heartWhite} /> : <div style={ vislble ? { backgroundImage: `url(${heart}?+${Math.random()})`} : null} className={styles.heart} />
+        }
+        <span onClick={arrayAdd}>数组自增</span>
+      </div> */}
+      <div className={styles.mainBox}>
+        <Carousel autoplay>
+          {imgList.map((item, i) => (
+            <div key={i}>
+              <div className={styles.carouselItem} style={{ backgroundImage: `url(${item})` }} />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </Fragment>
   );
